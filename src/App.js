467fee3844d16cd13f59d97102554caf27156f43
Srikanth Alladi <srikanth.05091993.com>
2022-03-15 15:47:58 +0530

    created project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './Homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+     <HomePage/>
+    </div>
+  );
+}
+
+export default App;
