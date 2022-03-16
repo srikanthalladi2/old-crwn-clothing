63bc0a94134e9812d671160640f283dda8587242
Srikanth Alladi <srikanth.05091993.com>
2022-03-16 12:36:21 +0530

    New Code merged

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './homepage.styles.scss';
+import directory from "../../components/directory/directory.component";
+import MenuItem from "../../components/menu-item/menu-item.component";
 
 const HomePage = () => {
     return(
